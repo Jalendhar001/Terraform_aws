f2c19e65af8d40e24738961e69d645cf92bc00d1
Jalendhar001 <120098371+Jalendhar001@users.noreply.github.com>
2023-02-06 15:50:00 +0530

    Update Backend.tf

--- Backend.tf
+++ Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-222"
+    bucket = "terraform1234567890"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
